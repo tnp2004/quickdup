7309ee9c2b56203d7465e1bbd519868920d343c9
tnp2004 <iceandiceisice2004@gmail.com>
2024-08-10 19:24:43 +0700

    feat(utils): hash and compare password

---
+++ pkg/utils/hash.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Printf("error encrypt password. Error: %s", err.Error())
+		return "", err
+	}
+
+	return string(b), nil
+}
+
+func ComparePassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
