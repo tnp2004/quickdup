4aebec3ee3f52f4def8fe4f62062475cd4fd047b
tnp2004 <iceandiceisice2004@gmail.com>
2024-08-16 19:32:19 +0700

    feat(migration): update generate code id with length of 6 characters

--- pkg/databases/migration/000001_create_tables.up.sql
+++ pkg/databases/migration/000001_create_tables.up.sql
@@ -11,6 +11,23 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION generate_unique_string()
+RETURNS TEXT LANGUAGE plpgsql AS $$
+DECLARE
+    new_string TEXT;
+BEGIN
+    LOOP
+        new_string := substring(md5(random()::text), 1, 6);
+        
+        IF NOT EXISTS (SELECT id FROM codes WHERE id = new_string) THEN
+            EXIT;
+        END IF;
+    END LOOP;
+    
+    RETURN new_string;
+END;
+$$;
+
 CREATE TABLE "users" (
   "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() not null,
   "username" varchar not null,
@@ -29,7 +46,7 @@ CREATE TABLE "notes" (
 );
 
 CREATE TABLE "codes" (
-  "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() not null,
+  "id" varchar PRIMARY KEY DEFAULT generate_unique_string() not null,
   "note_id" uuid not null,
   "expired_at" timestamp DEFAULT CURRENT_TIMESTAMP not null,
   "created_at" timestamp DEFAULT CURRENT_TIMESTAMP not null
@@ -42,14 +59,18 @@ CREATE TABLE "images" (
   "created_at" timestamp DEFAULT CURRENT_TIMESTAMP not null
 );
 
+CREATE TABLE "auth" (
+  "id" serial PRIMARY KEY not null,
+  "access_token" varchar not null,
+  "refresh_token" varchar not null,
+  "created_at" timestamp DEFAULT CURRENT_TIMESTAMP not null
+);
+
 ALTER TABLE "notes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 ALTER TABLE "images" ADD FOREIGN KEY ("note_id") REFERENCES "notes" ("id");
 ALTER TABLE "codes" ADD FOREIGN KEY ("note_id") REFERENCES "notes" ("id");
-
 
 CREATE TRIGGER set_updated_at BEFORE UPDATE ON users FOR EACH ROW EXECUTE FUNCTION set_updated_at_column();
 CREATE TRIGGER set_updated_at BEFORE UPDATE ON notes FOR EACH ROW EXECUTE FUNCTION set_updated_at_column();
 
 COMMIT;
-
-ROLLBACK;
