63c82becda00ca98c5ae4806010ee6183a48f89c
tnp2004 <iceandiceisice2004@gmail.com>
2024-08-18 15:03:04 +0700

    feat(auth): add authorization for health check

--- modules/servers/healthRouter.go
+++ modules/servers/healthRouter.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/tnp2004/quickdup/modules/auth/authMiddleware"
 )
 
 type healthResp struct {
@@ -15,8 +16,8 @@ type healthResp struct {
 func (s *Server) registerHealthRouter() {
 	r := s.server.Group("/api/v1/health")
 
-	r.GET("/server", s.serverHealth)
-	r.GET("/database", s.databaseHealth)
+	r.GET("/server", authMiddleware.Authorization(s.db, s.serverHealth))
+	r.GET("/database", authMiddleware.Authorization(s.db, s.databaseHealth))
 
 }
 
