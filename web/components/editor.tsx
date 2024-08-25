e8eef591236149fe347f9e12b123ef712c4df56f
tnp2004 <iceandiceisice2004@gmail.com>
2024-08-25 17:03:04 +0700

    refactor!: change api path from note to notes and remove title update

--- web/components/editor.tsx
+++ web/components/editor.tsx
@@ -4,6 +4,7 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Toolbar from "./toolbar";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Editor() {
   const editor = useEditor({
@@ -31,7 +32,7 @@ export default function Editor() {
         }
       }
     })],
-    content: "<p>Hello world</p>",
+    content: "",
     editorProps: {
         attributes: {
             class: "min-h-[500px] p-1 focus:outline-none"
@@ -40,11 +41,9 @@ export default function Editor() {
     immediatelyRender: false
   });
 
-  const [title, setTitle] = useState<string>("Untitled")
   const [code, setCode] = useState<string>("")
-
   const shareNote = async () => {
-    const data = await(await fetch("http://localhost:3000/api/create/notes", {
+    const data = await(await fetch(`http://localhost:3000/api/create/notes`, {
       method: "POST",
       body: JSON.stringify({
         userID: "",
@@ -61,23 +60,19 @@ export default function Editor() {
   return (
     <div>
       <div className="w-1/2 my-10 mx-auto flex flex-col justify-center">
-         <div className="flex justify-between">
-          <input className="text-3xl font-bold focus:outline-none bg-transparent" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-
-          <div className="flex gap-2">
+          <div className="flex gap-2 justify-end">
 
             <div className={`${code.length == 0 ? "hidden" : "block"} flex gap-1 my-auto`}>
            <span className="my-auto bg-gray-200 px-1 rounded-sm">{code}</span>
             <button onClick={copyToClipboard} className="text-gray-700 ring-1 ring-inset ring-gray-300 px-2 py-1 bg-white hover:bg-gray-50 rounded-md">
               <svg className="lucide lucide-copy w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
             </button>
-              <button className="text-gray-700 ring-1 ring-inset ring-gray-300 px-2 py-1 bg-white hover:bg-gray-50 rounded-md">
+              <Link href={`/notes?code=${code}`} className="text-gray-700 ring-1 ring-inset ring-gray-300 px-2 py-1 bg-white hover:bg-gray-50 rounded-md">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-link w-4"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>
-            </button>
+            </Link>
               </div>
               
             <button className="text-sm ring-1 ring-inset ring-gray-300 bg-white hover:bg-gray-50 rounded-md shadow-sm px-3 py-2 h-fit" onClick={shareNote}>share</button>
-          </div>
 
          </div>
           <Toolbar editor={editor} />
